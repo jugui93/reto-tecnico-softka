f5baf5cc803312c175baf2d263f36621a96e2f2f
jugui93 <juanguillermo93@gmail.com>
2022-06-11 07:56:20 -0500

    Actualización del readme, adición comentarios en codigo js

--- README.txt
+++ README.txt
@@ -1,8 +1,25 @@
-MDB5
-Version: FREE 4.1.0
+Reto Técnico- concurso de preguntas y respuestas.
+
+Se realiza la modelación de un concurso de preguntas y respuestas, se tiene un banco
+de 25 preguntas, identificadas por nivel del 1 al 5, en la primer ronda del juego el
+usuario gana 5 puntos si responde orrectamente, en la segunda gana 10 puntos, en la 
+tercera gana 15 puntos, en la cuarta 20 puntos, y en la ultima gana 25 puntos.
+
+Al iniciar, el concurso solicta el ingreso de un nombre de usuario, este nombre de 
+usuario es almacenado en localStorage del navegador. En pantalla se muestra una 
+pregunta del correspondiente al nivel 1, si el usuario responde correctamente, la 
+siguiente pregunta será del nivel 2, en caso de fallar en la respuesta se finaliza
+el juego, y se guarda el puntaje en localStorage, y se muestra en pantalla una 
+ventana con el resultado. 
+
+El concurso permite un maximo de 5 rondas de preguntas, l finalizar la quinta ronda
+de manera exitosa, se finaliza el juego, y se almacena el puntaje en el localStorage
+del navegador. 
+
+La maqueta de la aplicación se realizó en HTML5, el estilo visual se realiza con Bootstrap
+y CSS, la funcionalidad de la aplicación se programó en JavaScript. 
+
+Los archivos necesarios para la ejecución se encuentan en el repositorio. 
 
 Documentation:
 https://mdbootstrap.com/docs/standard/
-
-Contact:
-office@mdbootstrap.com
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Declaración de variables que contienen elementos del DOM
 const question = document.getElementById("question");
 const answers = Array.from(document.getElementsByClassName("answer-text"));
 const questionCounterText = document.getElementById("counter");
@@ -5,14 +6,14 @@ const scoreText = document.getElementById("score");
 const restart = document.getElementById("restart");
 const finish = document.getElementById("finish");
 const save = document.getElementById("save");
+// Declaración de variables
+let questionCounter;// Almacena el nivel de las preguntas
+let score;//Almacena el puntaje que va obtenido el usuario
+const MAX_LEVEL = 5;// Deefine # de rondas maximo del juego
+let acceptingAnswers;// Almacena booleano para no permitir mas de una respuesta seleccionada
+let availableQuestions; // Arreglo de 5 preguntas filtradas por nivel. 
 
-let questionCounter;
-let score;
-const MAX_LEVEL = 5;
-let acceptingAnswers;
-let availableQuestions;
-
-function loadFromFile() {
+function loadFromFile() { //Función para hacer petición de información al arcivo que contiene las preguntas
    let xhr = new XMLHttpRequest();
 
    xhr.open("GET", "js.json", false);
@@ -28,79 +29,78 @@ function loadFromFile() {
    return xhr.response;
 }
 
-let questions = JSON.parse(loadFromFile());
-const startGame = () => {
+let questions = JSON.parse(loadFromFile()); //Variable que almacena el banco de preguntas
+const startGame = () => { //Función encargada de iniciar el juego
    questionCounter = 0;
    score = 0;
    acceptingAnswers = true;
    scoreText.innerText = score;
-   registerUser();
+   registerUser(); //Función que abre ventana para ingresar el usuario
 
-   availableQuestions = getRandomQuestions(
+   availableQuestions = getRandomQuestions( // Función que permite obtener 5 preguntas ordenadas aleatoriamente 
       questions,
       MAX_LEVEL,
       questionCounter
    );
-   getNewQuestion();
+   getNewQuestion(); //Función que retorna una pregunta
 };
 
-const getRandomQuestions = (arr, m, n) => {
-   const questionsByLevel = [...arr].slice(m * n, m * n + 5);
-   const shuffled = [...questionsByLevel].sort(() => 0.5 - Math.random());
-   return shuffled;
+const getRandomQuestions = (arr, m, n) => { // Función que retorna un arreglo de 5 preguntas dependiendo del nivel en que se encuentra
+   const questionsByLevel = [...arr].slice(m * n, m * n + 5);// Toma 5 preguntas del banco de preguntas de acuerdo al nivel
+   const shuffled = [...questionsByLevel].sort(() => 0.5 - Math.random());// Organiza las preguntas de manera aleatoria
+   return shuffled; //retorna 5 preguntas ordenadas de manera aleatoria
 };
 
-const getNewQuestion = () => {
-   if (availableQuestions.length === 0) {
-      displayResults();
-      window.localStorage.setItem("Score", `${score}`);
+const getNewQuestion = () => { //Función encargada de mostrar en pantalla peguntas en cada ronda
+   if (availableQuestions.length === 0) { //condicional para finalizar el juego en la ronda 5
+      displayResults();// muestra ventana con el puntaje obtenido
+      window.localStorage.setItem("Score", `${score}`); //Almacenamiento delpuntje en localStorage
       return;
    }
 
-   questionCounter++;
-   questionCounterText.innerText = `${questionCounter}/${MAX_LEVEL}`;
+   questionCounter++;//Incrementa el nivel en cada ronda
+   questionCounterText.innerText = `${questionCounter}/${MAX_LEVEL}`;//Muestra el nivel en pantalla
+   currentQuestion = availableQuestions[0];//toma la primer pregunta del arreglo
+   question.innerText = currentQuestion.question;// Muestra la pregunta en pantalla
 
-   currentQuestion = availableQuestions[0];
-   question.innerText = currentQuestion.question;
-
-   answers.forEach((answer) => {
+   answers.forEach((answer) => { // Método que muestra en pantalla cada una de la opciones de respuesta
       answer.innerText = currentQuestion[answer.dataset["answer"]];
    });
 
-   answers.forEach((answer) => {
+   answers.forEach((answer) => { // Método que agrega manejador de eventos a cada pregunta
       answer.addEventListener("click", (e) => {
-         if (!acceptingAnswers) {
+         if (!acceptingAnswers) {//condicional para aceptar solo una respuesta
             return;
          }
          acceptingAnswers = false;
-         const clickedAnswer = e.target;
+         const clickedAnswer = e.target;//se identifica la preguta seleccionada
 
-         const answeredLetter = clickedAnswer.dataset["answer"];
+         const answeredLetter = clickedAnswer.dataset["answer"];// Se identifica la letra que se seleccionó como respuesta
 
-         let classToApply = "incorrect";
+         let classToApply = "incorrect";//Se define clase que se aplica or defecto al elemento seleccionado. 
 
-         if (answeredLetter === currentQuestion.answer) {
-            score += questionCounter * MAX_LEVEL;
-            scoreText.innerText = score;
-            classToApply = "correct";
-            clickedAnswer.parentElement.classList.add(classToApply);
+         if (answeredLetter === currentQuestion.answer) { //Condicional para respuesta correcta o incorrecta
+            score += questionCounter * MAX_LEVEL;//incremento del puntaje de acuerdo al nivel
+            scoreText.innerText = score;//actualización del puntaje en pantalla
+            classToApply = "correct";// Se define clase para el elemento seleccionado
+            clickedAnswer.parentElement.classList.add(classToApply);//Aplica clase al padre del elemento seleccionado
 
-            setTimeout(() => {
-               clickedAnswer.parentElement.classList.remove(classToApply);
-               availableQuestions = getRandomQuestions(
+            setTimeout(() => { // se define tiempo de espera de un segundo para pasar a la siguinte pregunta
+               clickedAnswer.parentElement.classList.remove(classToApply);//Remueve la clase aplicada
+               availableQuestions = getRandomQuestions(// Se actualizan las preuntas de acuerdo al nivel
                   questions,
                   MAX_LEVEL,
                   questionCounter
                );
-               getNewQuestion();
+               getNewQuestion(); //Se obtiene una nueva pregunta
                acceptingAnswers = true;
             }, 1000);
-         } else {
-            clickedAnswer.parentElement.classList.add(classToApply);
-            displayResults();
-            window.localStorage.setItem("Score", `${score}`);
-            setTimeout(() => {
-               clickedAnswer.parentElement.classList.remove(classToApply);
+         } else { //Cuando la respuesta es incorrecta se ejecuta esta sección 
+            clickedAnswer.parentElement.classList.add(classToApply);//Apica clase al elemento seleccionado
+            displayResults();// muestra el resultado en ventana
+            window.localStorage.setItem("Score", `${score}`);// almacena el puntaje del usuario
+            setTimeout(() => { //tiempo de espera para remover la clase aplicada al elemento
+               clickedAnswer.parentElement.classList.remove(classToApply);//Remueve la clase aplicada
             }, 2000);
             return;
          }
@@ -108,7 +108,13 @@ const getNewQuestion = () => {
    });
 };
 
-const displayResults = () => {
+const registerUser = () => { //función que muestra ventana de registro de usuario
+   const myModalEl = document.getElementById("myModal");
+   const modal1 = new mdb.Modal(userRegisterModal);
+   modal1.show();
+};
+
+const displayResults = () => { //función que muestra ventana con los resultados
    const myModalEl = document.getElementById("myModal");
    const modal = new mdb.Modal(endGameModal);
    const modalBody = document.getElementById("modal-body");
@@ -117,18 +123,11 @@ const displayResults = () => {
    acceptingAnswers = false;
 };
 
-const registerUser = () => {
-   const myModalEl = document.getElementById("myModal");
-   const modal1 = new mdb.Modal(userRegisterModal);
-   const modalBody = document.getElementById("modal-body");
-   modal1.show();
-};
-
-const saveRegistrer = () => {
+const saveRegistrer = () => {//Función que alamcena en localStorage nobre de usuario
    window.localStorage.setItem("User", nickname.value);
 };
 
-restart.addEventListener("click", startGame);
-finish.addEventListener("click", displayResults);
-save.addEventListener("click", saveRegistrer);
+restart.addEventListener("click", startGame); //Evento que reinicia el juego
+finish.addEventListener("click", displayResults); //Evento que finaliza el juego en cualquier momento
+save.addEventListener("click", saveRegistrer);//Evento que almacena nombre de usuario
 startGame();
